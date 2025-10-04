9cd3a4db829e1f047e60ac310ae2ea561ee9dd51
piyush ujgaokar <piyushujgaokar544@gmail.com>
2025-10-04 10:39:39 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/k72-Animated-Project/',
   plugins: [
     react(),
     tailwindcss(),
