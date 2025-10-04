12ed5989152f646a6abc7eb71ff791b273da6f45
piyush ujgaokar <piyushujgaokar544@gmail.com>
2025-10-04 11:38:58 +0530

    revert 1

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='w-full overflow-hidden h-full'>
-           <video className='h-full overflow-hidden loop w-full object-cover' autoPlay loop muted src="public/videos/k72 Video-1.mp4"></video>
+           <video className='h-full overflow-hidden loop w-full object-cover' autoPlay loop muted src="../../../public/videos/k72 Video-1.mp4"></video>
     </div>
   )
 }
